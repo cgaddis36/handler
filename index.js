3d4dffc6c8ec79514d0c1cfd2cd59b52bfa5de51
bobby <bobby@leapeasy.com>
2022-04-02 08:47:37 +0100

    update index file

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
 import logger from 'morgan';
 import Handler from './handler/index.js';
 
 const port = process.env.PORT || 8000;
 const app = express();
 
+app.use(helmet());
+
+// app.use(cors({ origin: environment_config.get('APP_URL'), credentials: true }));
+
+app.use(logger('common'));
+
+//Adding this to limit body size to 50kb thereby preventing large data based DOS attacks
+app.use(express.json({ limit: '50mb' }));
+
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(
+  bodyParser.urlencoded({
+    limit: '50mb',
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
+
 app.use(logger('common'));
 
 app.listen(port, () => {
-    console.log("Server started at port 2400");
+    console.log(`App running on port ${port}`);
     Handler();
   });
+
+app.get(['/', '/health'], async (request, response) => {
+  response.status(200).send('Success');
+});
